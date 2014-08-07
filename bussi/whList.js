81e96dd14c20097612f2ac56122cd4acf5901e76
lolandkidtress <lolandkidtress@gmail.com>
2014-08-07 14:37:10 +0800

    添加重复时间check，修改部分函数返回值的格式

--- bussi/whList.js
+++ bussi/whList.js
@@ -201,7 +201,7 @@ function getWHDetailList(questquery,response,callback){
 
             util.log('info','getWHDetailList returns');
             util.log('info',JSON.stringify(results));
-			      callback(results);
+			      callback(results[0]);
 			    }
 			    else
 			    {
@@ -349,7 +349,7 @@ function getPaidVCTime(questquery,response,callback){
   var avgHour;
   var LeftWH;
   var VCWH;
-
+  util.log('debug','getPaidVCTime get questquery:' + JSON.stringify(questquery));
   async.waterfall([
 
        function (callback){  //先取得考勤规则数据
@@ -374,12 +374,12 @@ function getPaidVCTime(questquery,response,callback){
 
                   callback(cb,null);
               }else{
-                if( util.jsonexist(cb[0],'/errno') && util.jsonget(cb[0],'/errno') == 200){   //非异常情况下返回
+                if( util.jsonexist(cb,'/errno') && util.jsonget(cb,'/errno') == 200){   //非异常情况下返回
 
                   util.log('debug','getPaidVCTime.getWHSetting OK ' + JSON.stringify(cb));
 
                   //计算一天的小时数
-                  avgHour = util.jsonget(cb[0],'/queryresult0/avgHour');
+                  avgHour = util.jsonget(cb,'/queryresult0/avgHour');
                   callback(null,avgHour);
                 }else{
                   cb = {
@@ -473,7 +473,7 @@ function getPaidVCTime(questquery,response,callback){
                }
               })
               .on('end', function(rows) {
-                console.log(LeftWH,VCWH);
+                util.log('info','合计有薪假：' + LeftWH + '已申请的有薪假合计：' + VCWH);
                   if(i>0){
 
                            util.jsonadd(result,'/errno','200');
@@ -518,7 +518,7 @@ function getPaidVCTime(questquery,response,callback){
           else
           {
             //global.queryDBStatus = 'err';
-            util.log('error',"err  = "+ sqlerr);
+            util.log('error',"err  = "+ JSON.stringify(sqlerr));
             results = sqlerr;
 
             util.log('info',JSON.stringify(results));
@@ -567,12 +567,12 @@ function getLieuVCTime(questquery,response,callback){
 
                   callback(cb,null);
               }else{
-                if( util.jsonexist(cb[0],'/errno') && util.jsonget(cb[0],'/errno') == 200){   //非异常情况下返回
+                if( util.jsonexist(cb,'/errno') && util.jsonget(cb,'/errno') == 200){   //非异常情况下返回
 
                   util.log('debug','getLieuVCTime.getWHSetting OK ' + JSON.stringify(cb));
 
                   //计算一天的小时数
-                  avgHour = util.jsonget(cb[0],'/queryresult0/avgHour');
+                  avgHour = util.jsonget(cb,'/queryresult0/avgHour');
                   callback(null,avgHour);
                 }else{
                   cb = {
@@ -674,7 +674,7 @@ function getLieuVCTime(questquery,response,callback){
                            util.jsonadd(results,'/LeftWHin30',LeftWHin30);
                            util.jsonadd(results,'/LeftDayin30',LeftWHin30/avgHour);
                            util.jsonadd(results,'/module','getLieuVCTimeHandle');
-                           console.log(results);
+                           //console.log(results);
                            callback(null,results);
 
                       }
@@ -1014,7 +1014,7 @@ function insertAccessRecord(questquery,response,callback){
                   util.jsonadd(insertArray,'/objdate',util.jsonget(questquery,'/queryresult'+i+'/objdate'));
                   util.jsonadd(insertArray,'/inTime',util.jsonget(questquery,'/queryresult'+i+'/inTime'));
                   util.jsonadd(insertArray,'/OutTime',util.jsonget(questquery,'/queryresult'+i+'/OutTime'));
-    
+
                   var sqlquery = Connection.query(insertSQL1, insertArray, function(err, result) {
                   //Connection.query('replace INTO test SET id="222212",res="eeee"', function(err, result) {
                     if (err) { 
@@ -1083,11 +1083,204 @@ function insertAccessRecord(questquery,response,callback){
 
 }
 
+//带有callback  根据传入的日期和时间，检查区间重复的考勤加班休假申请
+
+function getExistWHList(questquery,response,callback){
+  var i = 0;
+  var results ={
+    errno:'',
+    errmsg:''
+  };
+  var pool;
+  var Connection;
+  var selectSQL1;
+
+  async.series([
+      //取得每天详细的考勤记录
+      function(callback){
+      var sqlerr;
+      var row;
+      var err;
+      var res;
+
+      var pool = mysqlconn.poolConnection();
+
+      pool.getConnection(function(sqlerr, Connection) {
+        // connected! (unless `err` is set)
+        if(sqlerr!=null){
+          util.log('info','get ConnectPool error');
+          util.log('info',sqlerr);
+          err = sqlerr;
+          //util.jsonadd(err);
+          callback(sqlerr, null);
+
+        }else
+        {
+          //出勤申请 行列转换 方便后续计算是否满出勤
+        selectSQL1 = ' select EmployeeID,ObjYMD, ';
+        selectSQL1 += ' max(case whtype when "1" then formid else 0 end) whid, ';
+        selectSQL1 += ' max(case whtype when "1" then WH else 0 end) abswh, ';
+        selectSQL1 += ' max(case whtype when "1" then DtAppStatus else 0 end) whStatus, ';
+        selectSQL1 += ' max(case whtype when "1" then FlgHR else 0 end) whFlgHR, ';
+        selectSQL1 += ' max(case whtype when "1" then FromDt else 0 end) whFromDt, ';
+        selectSQL1 += ' max(case whtype when "1" then ToDt else 0 end) whToDt, ';
+        selectSQL1 += ' max(case whtype when "1" then PJInfoID else 0 end) whPJInfoID, ';
+        selectSQL1 += ' max(case whtype when "1" then FlgOut else 0 end) whFlgOut, ';
+        selectSQL1 += ' max(case whtype when "1" then FlgPJG else 0 end) whFlgPJG, ';
+
+        selectSQL1 += ' max(case whtype when "2" then formid else 0 end) ovid, ';
+        selectSQL1 += ' max(case whtype when "2" then WH else 0 end) ovwh, ';
+        selectSQL1 += ' max(case whtype when "2" then DtAppStatus else 0 end) ovStatus, ';
+        selectSQL1 += ' max(case whtype when "2" then FlgHR else 0 end) ovFlgHR, ';
+        selectSQL1 += ' max(case whtype when "2" then FromDt else 0 end) ovFromDt, ';
+        selectSQL1 += ' max(case whtype when "2" then ToDt else 0 end) ovToDt, ';
+        selectSQL1 += ' max(case whtype when "2" then PJInfoID else 0 end) ovPJInfoID, ';
+        selectSQL1 += ' max(case whtype when "2" then FlgOut else 0 end) ovFlgOut, ';
+        selectSQL1 += ' max(case whtype when "2" then FlgPJG else 0 end) ovFlgPJG, ';
+
+        selectSQL1 += ' max(case whtype when "3" then formid else 0 end) vcid, ';
+        selectSQL1 += ' max(case whtype when "3" then WH else 0 end) vcwh, ';
+        selectSQL1 += ' max(case whtype when "3" then DtAppStatus else 0 end) vcStatus, ';
+        selectSQL1 += ' max(case whtype when "3" then FlgHR else 0 end) vcFlgHR, ';
+        selectSQL1 += ' max(case whtype when "3" then FromDt else 0 end) vcFromDt, ';
+        selectSQL1 += ' max(case whtype when "3" then ToDt else 0 end) vcToDt, ';
+        selectSQL1 += ' max(case whtype when "3" then PJInfoID else 0 end) vcPJInfoID, ';
+        selectSQL1 += ' max(case whtype when "3" then FlgOut else 0 end) vcFlgOut, ';
+        selectSQL1 += ' max(case whtype when "3" then FlgPJG else 0 end) vcFlgPJG ';
+
+        selectSQL1 += ' from   ';
+        selectSQL1 += ' ( ';
+        selectSQL1 += ' select wh.WHFormID as formid,wh.EmployeeID,wh.ObjYMD,whd.WH as WH,wh.DtAppStatus,wh.FlgHR, ';
+        selectSQL1 += ' whd.FromDt,whd.ToDt,whd.PJInfoID,whd.FlgOut,whd.FlgPJG,1 as whtype ';
+        selectSQL1 += ' from trnwhform wh,trnwhformdetail whd ';
+        selectSQL1 += ' where wh.WHFormID = whd.WHFormID ';
+        selectSQL1 += ' and wh.DelFlg !=1 and whd.DelFlg!=1 ';
+        selectSQL1 += ' union all ';
+        selectSQL1 += ' select ov.OVFormID as formid,ov.EmployeeID,ov.ObjYMD,ov.OVWH as WH, ov.DtAppStatus,ov.FlgHR, ';
+        selectSQL1 += ' ovd.FromDt,ovd.ToDt,ovd.PJInfoID,ovd.FlgOut,ovd.FlgPJG,2 as whtype ';
+        selectSQL1 += ' from trnovform ov,trnovformdetail ovd ';
+        selectSQL1 += ' where ov.OVFormID = ovd.OVFormID ';
+        selectSQL1 += ' and ov.DelFlg !=1 and ovd.DelFlg!=1 ';
+        selectSQL1 += ' union all ';
+        selectSQL1 += ' select vc.VCFormID as formid,vc.EmployeeID,vcd.ObjYMD,vcd.VCTime as WH, vc.DtAppStatus,vcd.FlgHR, ';
+        selectSQL1 += ' vcd.VCFromDt,vcd.VCToDt,null as PJInfoID,null as FlgOut,null as FlgPJG,3 as whtype ';
+        selectSQL1 += ' from trnvcform vc,trnvcformdetail vcd ';
+        selectSQL1 += ' where vc.VCFormID = vcd.VCFormID ';
+        selectSQL1 += ' and vc.DelFlg !=1 and vcd.DelFlg!=1 ';
+        selectSQL1 += ' ) detail ';
+        selectSQL1 += ' where EmployeeID ='  + Connection.escape(util.jsonget(questquery,'/userid'))
+        selectSQL1 += ' and ObjYMD between ' + Connection.escape(util.jsonget(questquery,'/startdt')) + ' and ' + Connection.escape(util.jsonget(questquery,'/enddt'))
+        selectSQL1 += ' and ( ' + Connection.escape(util.jsonget(questquery,'/FromDt')) + ' <= ToDt and ' + Connection.escape(util.jsonget(questquery,'/ToDt'))  + ' > FromDt)';
+        selectSQL1 += ' group by detail.EmployeeID,ObjYMD ';
+
+        util.log('debug',selectSQL1);
+
+            var query = Connection.query(selectSQL1);
+            query
+              .on('error', function(sqlerr) {
+                // Handle error, an 'end' event will be emitted after this as well
+                results = sqlerr;
+                util.log('info','Connect error '+ sqlerr);
+                util.jsonadd(results,'/sqlstmt',selectSQL1);
+                callback(sqlerr,null);
+
+              })
+              .on('result', function(rows) {
+                // Pausing the connnection is useful if your processing involves I/O
+                //connection.pause();
+                util.jsonadd(results,'/queryresult'+i+'/EmployeeID',rows.EmployeeID);
+                util.jsonadd(results,'/queryresult'+i+'/ObjYMD',rows.ObjYMD);
+
+                util.jsonadd(results,'/queryresult'+i+'/whid',rows.whid);
+                util.jsonadd(results,'/queryresult'+i+'/whtype',rows.whtype);
+                util.jsonadd(results,'/queryresult'+i+'/abswh',rows.abswh);
+                util.jsonadd(results,'/queryresult'+i+'/whStatus',rows.whStatus);
+                util.jsonadd(results,'/queryresult'+i+'/whFlgHR',rows.whFlgHR);
+                util.jsonadd(results,'/queryresult'+i+'/whFromDt',rows.whFromDt);
+                util.jsonadd(results,'/queryresult'+i+'/whToDt',rows.whToDt);
+                util.jsonadd(results,'/queryresult'+i+'/whPJInfoID',rows.whPJInfoID);
+                util.jsonadd(results,'/queryresult'+i+'/whFlgOut',rows.whFlgOut);
+                util.jsonadd(results,'/queryresult'+i+'/whFlgPJG',rows.whFlgPJG);
+
+                util.jsonadd(results,'/queryresult'+i+'/ovwh',rows.ovwh);
+                util.jsonadd(results,'/queryresult'+i+'/ovid',rows.ovid);
+                util.jsonadd(results,'/queryresult'+i+'/ovStatus',rows.ovStatus);
+                util.jsonadd(results,'/queryresult'+i+'/ovFlgHR',rows.ovFlgHR);
+                util.jsonadd(results,'/queryresult'+i+'/ovFromDt',rows.ovFromDt);
+                util.jsonadd(results,'/queryresult'+i+'/ovToDt',rows.ovToDt);
+                util.jsonadd(results,'/queryresult'+i+'/ovPJInfoID',rows.ovPJInfoID);
+                util.jsonadd(results,'/queryresult'+i+'/ovFlgOut',rows.ovFlgOut);
+                util.jsonadd(results,'/queryresult'+i+'/ovFlgPJG',rows.ovFlgPJG);
+
+                util.jsonadd(results,'/queryresult'+i+'/vcid',rows.vcid);
+                util.jsonadd(results,'/queryresult'+i+'/vcwh',rows.vcwh);
+                util.jsonadd(results,'/queryresult'+i+'/vcStatus',rows.vcStatus);
+                util.jsonadd(results,'/queryresult'+i+'/vcFlgHR',rows.vcFlgHR);
+                util.jsonadd(results,'/queryresult'+i+'/vcFromDt',rows.vcFromDt);
+                util.jsonadd(results,'/queryresult'+i+'/vcToDt',rows.vcToDt);
+                util.jsonadd(results,'/queryresult'+i+'/vcPJInfoID',rows.vcPJInfoID);
+                util.jsonadd(results,'/queryresult'+i+'/vcFlgOut',rows.vcFlgOut);
+                util.jsonadd(results,'/queryresult'+i+'/vcFlgPJG',rows.vcFlgPJG);
+                //util.jsonadd(results,'/queryresult'+i+'/uuid',rows.uuid_);
+                i=i+1;
+              })
+              .on('end', function(rows) {
+
+                  if(i>0){
+                    util.jsonadd(results,'/errno','200');
+                    util.jsonadd(results,'/errmsg','getExistWHList complete');
+                    util.jsonadd(results,'/rowcount',i);
+                    util.jsonadd(results,'/module','getExistWHList');
+                  }
+                  else
+                  {
+                    util.jsonadd(results,'/errno','300');
+                    util.jsonadd(results,'/errmsg','getExistWHList Empty');
+                    util.jsonadd(results,'/rowcount',0);
+                    util.jsonadd(results,'/module','getExistWHList');
+                  }
+
+                  Connection.release();
+                  callback(null,results);
+              });
+        }
+
+      });
+  }
+
+
+        ],function(sqlerr,results){
+
+          if(sqlerr == null||sqlerr == '' ){
+
+
+            util.log('info','getExistWHList returns');
+            util.log('info',JSON.stringify(results[0]));
+            callback(results[0]);
+          }
+          else
+          {
+            //global.queryDBStatus = 'err';
+            util.log('error',"err  = "+ sqlerr);
+            results = sqlerr;
+            util.jsonadd(results,'/errno','400');
+            util.jsonadd(results,'/errmsg','getExistWHList error');
+              // util.jsonadd(results,'/rowcount',i);
+            util.jsonadd(results,'/module','getExistWHList');
+            util.log('info','getExistWHList returns');
+            util.log('info',JSON.stringify(results));
+            callback(results);
+          }
+      }); //async.series end
+
+}
+
 
 
 
 
 exports.getWHDetailList=getWHDetailList;
+exports.getExistWHList=getExistWHList;
 exports.getTotalVCTime =getTotalVCTime;
 exports.getTotalOVTime =getTotalOVTime;
 exports.getPaidVCTime =getPaidVCTime;
